bb99082496d8cdc5e340fb85e1d11645df0f7dc3
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-08 16:19:07 +0530

    Second commit

---
+++ arrays.rb
@@ -0,0 +1,54 @@
+#array Creation
+fruits=["orange","pineapple","chickoo","mango"]
+puts fruits[1] #pineapple
+
+puts fruits.first
+puts fruits.last
+#pop removes last item
+puts fruits.pop
+puts fruits
+
+#addition in last by push or <<
+fruits.push("grapes");
+fruits<<"Guvava"
+
+puts fruits
+
+p fruits.shift #removes first elem
+
+p fruits
+
+fruits.reverse
+ p fruits
+
+ #flatten the array reducing it to 1 d
+ s = [ 1, 2, 3 ]           #=> [1, 2, 3]
+t = [ 4, 5, 6, [7, 8] ]   #=> [4, 5, 6, [7, 8]]
+a = [ s, t, 9, 10 ]       #=> [[1, 2, 3], [4, 5, 6, [7, 8]], 9, 10]
+b=a.flatten                #=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p b
+
+#! is a destructible return type
+
+p fruits.shuffle!
+p fruits.shuffle!
+p fruits.shuffle!
+
+a.flatten!
+p a
+
+#map method
+puts "Map Methods"
+
+fruits2=fruits.map do |fruit|
+    fruit #last expresssion equivalent to return fruit
+end
+
+p "Fruits 2:", fruits2
+
+nums=[1,2,3,4,5,6]
+p nums
+squares=nums.map do |n|
+     n*n
+end
+p squares
