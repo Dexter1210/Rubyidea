7118bbde27212c6e7d86b051ca95956e5b6455f6
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-09 16:03:55 +0530

    Static method,static and global variables

--- class-concepts.rb
+++ class-concepts.rb
@@ -1,4 +1,10 @@
+#for creating global variables
+$everywhere="I am global"
+
 class ActiveRecord
+
+    # @@ for creating class level or static variables
+    @@count=1
     p self
     puts "Hmm... Is it strange" #direct invocation -> macro methods
 
@@ -7,13 +13,18 @@ class ActiveRecord
         p self
         puts "Find all record"
      end
-
+     #use of self. for static method
      def self.all1
         puts "This is an imitation of static method"
+     end
+
+     def self.count #getter method for static variable
+        @@count
      end
 end
 
 ar=ActiveRecord.new
 ar.all
 ActiveRecord.all1
+p ActiveRecord.count
 
