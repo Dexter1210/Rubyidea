39783349751f335301b28cd846960549e9adb9b4
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-10 14:48:42 +0530

    Meta Programming-1

--- metaprogramming/cat-app.rb
+++ metaprogramming/cat-app.rb
@@ -3,8 +3,11 @@ class Object
     #Overriding
     def self.const_missing(c)
         puts "#{c} is missing. so lets load it dynamically"
-        require_relative "./cat"
-        Cat
+        require_relative "./#{c.downcase}"
+       # Cat
+      klass= const_get(c)
+      p klass
+      klass
     end
 end
 
@@ -12,3 +15,4 @@ end
 
 puffy_cat=Cat.new
 puffy_cat.talk
+
