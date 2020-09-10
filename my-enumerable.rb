a658fd15ced7ebb5ffa629fe447a1d65004d5ad7
Dexter1210 <deveshsharma1210@gmail.com>
2020-09-10 12:41:07 +0530

    Customizable Enumerables

---
+++ my-enumerable.rb
@@ -0,0 +1,46 @@
+module MyEnumerable
+    def map (&block)
+      result = []   # local variable 
+      each do |ele|
+        result << block.call(ele)
+      end
+      result
+    end
+  
+    def find (&block)
+      result = nil
+      found = false 
+  
+      each do |ele|
+        if block.call(ele) 
+          result = ele 
+          found = true
+          break
+        end
+      end
+      found ? result : nil
+    end
+  
+    def first 
+      found = nil 
+      each do |ele|
+        found = ele
+        break
+      end
+      found
+    end
+  
+    def select(&block)
+        result =[]
+        self.each do |elem|
+            result << elem if block.call(elem) == true
+         end
+        result
+    end
+    
+  
+    def find_all 
+    end
+  
+    #.....
+  end
